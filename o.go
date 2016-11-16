ed6d399a00f3e00f515c824046b7f055bbe8376c
tamal <tamal@appscode.com>
2016-11-16 00:59:56 -0800

    Add block comments.

--- o.go
+++ o.go
@@ -7,22 +7,40 @@ import (
 )
 
 func DumpHttpRequest(req *http.Request) {
+	fmt.Println()
+	fmt.Println("REQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ----------------------------------------------------")
 	b, err := httputil.DumpRequest(req, true)
 	if err == nil {
 		fmt.Println(string(b))
+	} else {
+		fmt.Println(err)
 	}
+	fmt.Println("----------------------------------------------------REQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ")
+	fmt.Println()
 }
 
 func DumpHttpRequestOut(req *http.Request) {
+	fmt.Println()
+	fmt.Println("REQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ----------------------------------------------------")
 	b, err := httputil.DumpRequestOut(req, true)
 	if err == nil {
 		fmt.Println(string(b))
+	} else {
+		fmt.Println(err)
 	}
+	fmt.Println("----------------------------------------------------REQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ")
+	fmt.Println()
 }
 
 func DumpHttpResponse(resp *http.Response) {
+	fmt.Println()
+	fmt.Println("RESPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP----------------------------------------------------")
 	b, err := httputil.DumpResponse(resp, true)
 	if err == nil {
 		fmt.Println(string(b))
+	} else {
+		fmt.Println(err)
 	}
+	fmt.Println("----------------------------------------------------RESPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP")
+	fmt.Println()
 }
