4ca214c0460d9077e49e3ef394785f92408f7699
tamal <tamal@appscode.com>
2016-12-27 00:13:21 -0800

    Allow printing msg.

--- runtime.go
+++ runtime.go
@@ -6,10 +6,13 @@ import (
 	"runtime"
 )
 
-func FILE() {
+func FILE(a ...interface{}) {
 	_, file, ln, ok := runtime.Caller(1)
 	if ok {
 		fmt.Println("__FILE__", file, "__LINE__", ln)
+		if len(a) > 0 {
+			fmt.Println(a...)
+		}
 	} else {
 		log.Fatal("Failed to detect runtime caller info.")
 	}
