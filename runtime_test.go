bcec1af62911e852baddf07214f883258c2c179e
the-redback <maruf@appscode.com>
2017-11-13 12:05:00 +0600

    tests

--- runtime_test.go
+++ runtime_test.go
@@ -16,4 +16,40 @@ func TestDumpJson(t *testing.T) {
 	PrettyJson(str,"Asad")
 	PrettyJson(str,"Asad","jacob")
 	PrettyJson(str)
+//===========================================
+	type Salary struct{
+		Basic, HRA, TA float64
+	}
+
+	type Employee struct{
+		FirstName, LastName, Email string
+		Age int
+		MonthlySalary []Salary
+	}
+
+	e := Employee{
+		FirstName: "Mark",
+		LastName: "Jones",
+		Email: "mark@gmail.com",
+		Age: 25,
+		MonthlySalary: []Salary{
+			Salary{
+				Basic:15000.00,
+				HRA:5000.00,
+				TA:2000.00,
+			},
+			Salary{
+				Basic:16000.00,
+				HRA:5000.00,
+				TA:2100.00,
+			},
+			Salary{
+				Basic:17000.00,
+				HRA:5000.00,
+				TA:2200.00,
+			},
+		},
+	}
+
+	PrettyJson(e,"Mark Jones")
 }
